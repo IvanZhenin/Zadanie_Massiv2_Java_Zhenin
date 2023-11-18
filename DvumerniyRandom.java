75b96cd373a3b3bbdad5ca21c3574467b21c9542
GassonDee <130245874+GassonDee@users.noreply.github.com>
2023-11-18 23:03:57 +0500

    Add files via upload

---
+++ DvumerniyRandom.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class New
+{
+
+    public static void main(String[] args) 
+    {
+   	 Scanner scanner = new Scanner(System.in);
+    	// Ввод числа столбцов и строк
+    	System.out.println("Введите количество столбцов:");
+    	int columnCount = scanner.nextInt();
+    	System.out.println("Введите количество строк:");
+    	int rowCount = scanner.nextInt();
+
+		int[][] array = new int[rowCount][columnCount];
+    	// Присвоение случайных значений элементам массива
+    	for (int i = 0; i < rowCount; i++) 
+    	{
+            for (int j = 0; j < columnCount; j++) 
+            {
+              array[i][j] = (int) (Math.random() * 10);
+            }
+    	 }
+
+    	System.out.print("Матрица:\n");
+    	// Вывод
+    	for (int[] row : array) 
+    	{
+      	   for (int number : row) 
+           {
+             System.out.printf("%d ", number);
+           }
+       	   System.out.println();
+    	}
+    }
+}
